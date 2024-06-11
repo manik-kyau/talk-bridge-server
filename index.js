4750615026ab7fcec6749cc1a961ccf6fdaf4c7d
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-06-11 13:40:41 +0600

    user delete by admin in database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,11 @@ async function run() {
     const announcementCollection = client.db("talkbridgeDB").collection("announcements");
 
     // users relaated api
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -42,8 +47,15 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+    // delete user
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
-    // post related api
+    // Posts related api
     app.get('/posts', async (req, res) => {
       const result = await postCollection.find().toArray();
       res.send(result);
